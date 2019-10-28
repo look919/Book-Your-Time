4e1d598079f24d4e93d01cbe7e373396cc27528d
Wirkus Tomasz <wirkus919@gmail.com>
2019-10-28 21:45:15 +0100

    Added basic styles for components

--- src/icons/icons.js
+++ src/icons/icons.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 export const LogoIcon = props => (
-    <svg className="header__nav__item" width="5rem" height="5rem">
+    <svg className="header__photo" width="5rem" height="5rem">
         <path d="M14 22h4v4h-4zM22 8c1.105 0 2 0.895 2 2v6l-6 4h-4v-2l6-4v-2h-10v-4h12zM16 3c-3.472 0-6.737 1.352-9.192 3.808s-3.808 5.72-3.808 9.192c0 3.472 1.352 6.737 3.808 9.192s5.72 3.808 9.192 3.808c3.472 0 6.737-1.352 9.192-3.808s3.808-5.72 3.808-9.192c0-3.472-1.352-6.737-3.808-9.192s-5.72-3.808-9.192-3.808zM16 0v0c8.837 0 16 7.163 16 16s-7.163 16-16 16c-8.837 0-16-7.163-16-16s7.163-16 16-16z"></path>   
     </svg>
     )    
 
 export const BookIcon = props => (
-    <svg className="header__nav__item header__nav__item__photo--book" width="5rem" height="4rem">
+    <svg className="" width="5rem" height="4rem">
         <path d="M28 4v26h-21c-1.657 0-3-1.343-3-3s1.343-3 3-3h19v-24h-20c-2.2 0-4 1.8-4 4v24c0 2.2 1.8 4 4 4h24v-28h-2z"></path>
         <path d="M7.002 26v0c-0.001 0-0.001 0-0.002 0-0.552 0-1 0.448-1 1s0.448 1 1 1c0.001 0 0.001-0 0.002-0v0h18.997v-2h-18.997z"></path>
     </svg>
