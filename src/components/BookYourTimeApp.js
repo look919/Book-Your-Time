2c422bed0f1b56c0d3a7772fd2d5ca0f60387aec
Wirkus Tomasz <wirkus919@gmail.com>
2019-10-29 19:25:36 +0100

    Added button with its styles, added few items to database

--- src/components/BookYourTimeApp.js
+++ src/components/BookYourTimeApp.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Header from './Header'
+import '../firebase/firebase'
 
 export default class BookYourTimeApp extends React.Component {
     state={
         bookDrawn: undefined
     }
-
+    handleRandomPick = () =>{
+        const randomNum = Math.floor(Math.random() * this.state.options.length)
+        const option = this.state.options[randomNum]
+        this.setState(() => ({
+            selectedOption: option
+        }))
+    }
 
 
     render(){
@@ -16,9 +23,10 @@ export default class BookYourTimeApp extends React.Component {
                 <Header />
                 <main className="home-page">
                     <h1 className="home-page__heading-primary">No idea what to do?</h1>
-                    <p className="home-page__text">Looking for something to read but you have no idea what? You couldn't find a better place to look for it. Just choose category then click the button bellow and i will find something for you, I guarantee you won't regret it!</p>
+                    <p className="home-page__text">Looking for something to read but you have no idea what? You couldn't find a better place to look for it. Just click the button bellow and i will find something for you, I guarantee you won't regret it!</p>
+                    <button className="button">Book Your Time</button>
                 </main>
-            
+                
             
             
             
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,49 +2,26 @@ import * as firebase from 'firebase'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCEhxfQfYd6LNkxmga74bFh9cEbar33NKc",
-    authDomain: "chooseyourdestiny.firebaseapp.com",
-    databaseURL: "https://chooseyourdestiny.firebaseio.com",
-    projectId: "chooseyourdestiny",
-    storageBucket: "chooseyourdestiny.appspot.com",
-    messagingSenderId: "531558910019",
-    appId: "1:531558910019:web:9bce9932e3b946f11dacf6",
-    measurementId: "G-31RERYS5H1"
-  };
-  
+  apiKey: "AIzaSyAT1bggYtZw7lYp4PUcTVo3IOLm9M0n0hg",
+  authDomain: "bookyourtime-bd403.firebaseapp.com",
+  databaseURL: "https://bookyourtime-bd403.firebaseio.com",
+  projectId: "bookyourtime-bd403",
+  storageBucket: "bookyourtime-bd403.appspot.com",
+  messagingSenderId: "934837037234",
+  appId: "1:934837037234:web:5b7c1047f5c96610a6c1ab",
+  measurementId: "G-G0ZBEC4KHL"
+};
+
+
 firebase.initializeApp(firebaseConfig);
-const database = firebase.database()
 
-export {firebase, database as default}
-
-
-  
-// <!-- The core Firebase JS SDK is always required and must be listed first -->
-// <script src="https://www.gstatic.com/firebasejs/7.2.2/firebase-app.js"></script>
-
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-//      https://firebase.google.com/docs/web/setup#available-libraries -->
-// <script src="https://www.gstatic.com/firebasejs/7.2.2/firebase-analytics.js"></script>
-
-// <script>
-
-  
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-//   firebase.analytics();
-// </script>
-
-//   firebase.initializeApp(firebaseConfig);
-//   const database = firebase.database()
-
-
-
-//   export {firebase, database as default}
-
-
-
-
-
-
+// firebase.database().ref('books').push({
+//   id: 10,
+//   title: 'Les Misérables',
+//   releaseDate: '1862',
+//   author: 'Victor Hugo',
+//   description: 'Examining the nature of law and grace, the novel elaborates upon the history of France, the architecture and urban design of Paris, politics, moral philosophy, antimonarchism, justice, religion, and the types and nature of romantic and familial love. Les Misérables has been popularized through numerous adaptations for film, television and the stage, including a musical.',
+//   url: 'https://en.wikipedia.org/wiki/Les_Mis%C3%A9rables#/media/File:Monsieur_Madeleine_par_Gustave_Brion.jpg'
+// })
 
 
