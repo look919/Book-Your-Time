f81e3b46edb3821cb29107c06956034fd5ba54fc
Wirkus Tomasz <wirkus919@gmail.com>
2019-10-29 22:07:27 +0100

    added styles for modal component

--- src/components/BookModal.js
+++ src/components/BookModal.js
@@ -17,7 +17,26 @@ const BookModal = (props) => {
             closeTimeoutMS={200}
             className="modal"
             >
-            {props.bookDrawn !== undefined &&<h2> {props.bookDrawn.author}</h2>}
+            <h1 className="modal__heading-primary">Thats what I found for you!</h1>
+            <div className="modal__content">
+                <div className="modal__info">
+                { props.bookDrawn !== undefined && <h2 className="modal__info__title">{props.bookDrawn.title}</h2> }
+                    <div className="modal__info__subtitle">
+                        { props.bookDrawn !== undefined && <p className="modal__info__subtitle--author">Author: {props.bookDrawn.author}</p> }
+                        { props.bookDrawn !== undefined && <p className="modal__info__date">Release year: {props.bookDrawn.releaseDate}</p> }
+                    </div>
+                    <div className="modal__info__description">
+                        { props.bookDrawn !== undefined && <p className="modal__info__date">{props.bookDrawn.description}</p> }
+                    </div>
+                </div>
+                <div className="modal__photo">
+                { props.bookDrawn !== undefined && <img src={props.bookDrawn.url} alt="book's photo"/> }
+                </div>
+            </div>
+            
+            
+            
+
             
             </Modal>
         )
