9ff8eb51660d9548d81e11f399e59863796a36b0
Wirkus Tomasz <wirkus919@gmail.com>
2019-10-29 20:41:11 +0100

    Added connection beetwen state and firebase

--- src/components/BookYourTimeApp.js
+++ src/components/BookYourTimeApp.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import Header from './Header'
-import '../firebase/firebase'
+import database from '../firebase/firebase'
 
 export default class BookYourTimeApp extends React.Component {
     state={
         bookDrawn: undefined
     }
     handleRandomPick = () =>{
-        const randomNum = Math.floor(Math.random() * this.state.options.length)
-        const option = this.state.options[randomNum]
-        this.setState(() => ({
-            selectedOption: option
+        const randomNum = Math.floor(Math.random() * 10+1)
+        console.log(randomNum)
+        
+        database.ref('books').on('value',(snapshot)=>{
+            snapshot.forEach((childSnapshot) => {
+                if(childSnapshot.val().id === randomNum){
+                    console.log(childSnapshot.val())
+                    this.state.bookDrawn = childSnapshot.val()
+                    console.log(this.state.bookDrawn)
+                }
+            })
+
+        })
+        //console.log(this.state.bookDrawn)
+    }
+    handleClearSelectedOption = () =>{
+        this.setState(()=> ({
+            bookDrawn: undefined
         }))
     }
 
@@ -24,7 +38,7 @@ export default class BookYourTimeApp extends React.Component {
                 <main className="home-page">
                     <h1 className="home-page__heading-primary">No idea what to do?</h1>
                     <p className="home-page__text">Looking for something to read but you have no idea what? You couldn't find a better place to look for it. Just click the button bellow and i will find something for you, I guarantee you won't regret it!</p>
-                    <button className="button">Book Your Time</button>
+                    <button onClick ={this.handleRandomPick} className="button">Book Your Time</button>
                 </main>
                 
             
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,6 +14,18 @@ const firebaseConfig = {
 
 
 firebase.initializeApp(firebaseConfig);
+const database = firebase.database()
+
+
+export {firebase, database as default}
+
+
+
+
+
+
+
+
 
 // firebase.database().ref('books').push({
 //   id: 10,
